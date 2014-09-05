6a8e97ae7a2e83945d06ced0fb12976a45b6c80f
dal <dal.adamson@gmail.com>
2014-09-04 20:48:16 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tmbc::Application.config.secret_key_base = '6cf66b41663beab57ad624b0d1a14981bfa6a54cdc84c372bac3834132da1576a5377e08936f512ff00999255df7d2333a594a398ad19a5e816fe53a7dc97435'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tmbc::Application.config.session_store :cookie_store, key: '_tmbc_session'
