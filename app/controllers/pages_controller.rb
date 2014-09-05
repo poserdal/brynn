5a9aba87cafe77042e8e205bd38718de9cf00830
dal <dal.adamson@gmail.com>
2014-09-04 21:16:08 -0700

    added some pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,13 @@
+class PagesController < ApplicationController
+  def welcome
+  end
+
+  def thanks
+  end
+
+  def contact
+  end
+
+  def about
+  end
+end
---
+++ test/controllers/pages_controller_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class PagesControllerTest < ActionController::TestCase
+  test "should get welcome" do
+    get :welcome
+    assert_response :success
+  end
+
+  test "should get thanks" do
+    get :thanks
+    assert_response :success
+  end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+  end
+
+  test "should get about" do
+    get :about
+    assert_response :success
+  end
+
+end
