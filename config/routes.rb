01229aa070a2fdab4279d684cf61f39b54bc6840
dal <dal.adamson@gmail.com>
2014-09-10 12:42:16 -0700

    fixed routes & title tags

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Tmbc::Application.routes.draw do
   root "pages#welcome"
   get "thanks" => "pages#thanks"
   get "contact" => "pages#contact"
-  get "about" => "pages#contact"
+  get "about" => "pages#about"
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
